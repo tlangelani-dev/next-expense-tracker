afe1afe53b5c698ff108dfcaa35effc657cb8876
Tlangelani Mboweni <mbowenit@gmail.com>
2023-12-15 16:49:05 +0200

    updates

--- app/components/CreateForm.jsx
+++ app/components/CreateForm.jsx
@@ -5,21 +5,23 @@ const CreateForm = () => {
         <div className="container mx-auto p-6">
             <form action={addExpense}>
                 <div className="block mb-2">
-                    <label htmlFor="title" className="block mb-1">Title</label>
-                    <input className="appearance-none border rounded w-full p-1" type="text" id="title" name="title" required />
+                    <label htmlFor="title" className="block mb-1 text-gray-600">Title</label>
+                    <input className="appearance-none border rounded w-full p-2" type="text" id="title" name="title" required />
                 </div>
                 <div className="block mb-2">
-                    <label htmlFor="type" className="block mb-1">Type</label>
-                    <select className="w-full border rounded p-1" name="type" id="type">
+                    <label htmlFor="type" className="block mb-1 text-gray-600">Type</label>
+                    <select className="w-full border rounded p-2" name="type" id="type">
                         <option value="Deposit">Deposit</option>
                         <option value="Withdraw">Withdraw</option>
                     </select>
                 </div>
                 <div className="block mb-2">
-                    <label htmlFor="amount" className="block mb-1">Amount</label>
-                    <input className="appearance-none border rounded w-full p-1" type="number" id="amount" name="amount" required />
+                    <label htmlFor="amount" className="block mb-1 text-gray-600">Amount</label>
+                    <input className="appearance-none border rounded w-full p-2" type="number" id="amount" name="amount" required />
                 </div>
-                <button className="bg-slate-700 hover:bg-slate-900 transition text-white py-2 px-4 rounded my-3" type="submit">Submit</button>
+                <button className="bg-slate-700 hover:bg-slate-900 transition text-white py-2 px-4 rounded my-3" type="submit">
+                    Add
+                </button>
             </form>
         </div>
     )
--- app/page.jsx
+++ app/page.jsx
@@ -4,12 +4,15 @@ import ExpenseList from '@/app/components/ExpenseList';
 const Home = () => {
     return (
         <div className="min-h-screen">
-            <div className="container mx-auto p-6">
-                <h1>My Expenses</h1>
-                <form action="/auth/logout" method="POST">
-                    <button className="bg-gray-600 hover:bg-gray-700 text-white font-bold py-2 px-4" type="submit">Logout</button>
-                </form>
-            </div>
+            <header className="bg-slate-800 text-white">
+                <div className="container mx-auto p-6 flex justify-between">
+                    <h1 className="font-bold">Expenses</h1>
+                    <form action="/auth/logout" method="POST">
+                        <button className="bg-gray-600 hover:bg-gray-700 transition duration-300 text-white rounded text-sm py-2 px-4" type="submit">Logout</button>
+                    </form>
+                </div>
+            </header>
+            
             <CreateForm />
             <ExpenseList />
         </div>
